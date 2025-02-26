e2cae44485f8f8219c73e98158c951aac30ad2eb
Artem Prygunov <harry177@rambler.ru>
2025-02-26 17:31:02 +0100

    feat: complete mobile layout and app logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 import "./header.scss";
 
 export const Header = () => {
-  const mobile = useMediaQuery("(max-width: 767px)");
+  const mobile = useMediaQuery("(max-width: 729px)");
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className="header">
@@ -11,7 +14,16 @@ export const Header = () => {
           <img src="logo-1.svg"></img>
           <img src="logo-2.svg"></img>
         </div>
-        {!mobile && (
+        {mobile ? (
+          <button
+            className={`burger-button ${isOpen ? "burger-button--open" : ""}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className="burger-button__line" />
+            <div className="burger-button__line" />
+            <div className="burger-button__line" />
+          </button>
+        ) : (
           <nav>
             <ul className="nav-list">
               <li>Home</li>
@@ -23,6 +35,17 @@ export const Header = () => {
           </nav>
         )}
       </div>
+      {isOpen && (
+        <nav className="header__mobile-layer">
+          <ul className="nav-list__mobile">
+            <li className="header__mobile-section">Home</li>
+            <li className="header__mobile-section">Products</li>
+            <li className="header__mobile-section">
+              <div className="nav-cart"></div>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
--- src/components/JourneySection/JourneySection.tsx
+++ src/components/JourneySection/JourneySection.tsx
@@ -4,19 +4,22 @@ export const JourneySection = () => {
   return (
     <section className="journey">
       <h2>Embark on a space journey</h2>
-      <span>
-        Travelling into space is one of the most exciting and unforgettable
-        adventures that can change your life forever. And if you have ever
-        dreamed of exploring stars, planets and galaxies, then our company is
-        ready to help you realize this dream. We offer a unique experience that
-        will allow you to go on a space journey and see all the secrets of the
-        universe. We guarantee that every moment in space will be filled with
-        incredible impressions, excitement and new discoveries. Our team of
-        professionals takes care of your safety and comfort so that you can
-        fully enjoy your adventure in space. We offer various options for space
-        excursions.
-      </span>
-      <button className="journey-button">Read more</button>
+      <input type="checkbox" id="read-more" className="journey__read-more-toggle" />
+  <div className="journey__text-container">
+    <span className="journey__text">
+      Travelling into space is one of the most exciting and unforgettable
+      adventures that can change your life forever. And if you have ever
+      dreamed of exploring stars, planets and galaxies, then our company is
+      ready to help you realize this dream. We offer a unique experience that
+      will allow you to go on a space journey and see all the secrets of the
+      universe. We guarantee that every moment in space will be filled with
+      incredible impressions, excitement and new discoveries. Our team of
+      professionals takes care of your safety and comfort so that you can
+      fully enjoy your adventure in space. We offer various options for space
+      excursions.
+    </span>
+  </div>
+  <label htmlFor="read-more" className="journey-button"></label>
     </section>
   );
 };
